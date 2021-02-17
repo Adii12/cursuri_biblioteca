632516f7ae9a5ce1d3df04e0dfdeae8ff86b6309
Adrian Cimpean <cimpean_adrian12@yahoo.com>
2021-02-17 10:58:50 +0200

    Added CRUD operations

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,59 @@
 class BooksController < ApplicationController
+
   def index
     @books = Book.all
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(params.require(:book).permit(:title, :body))
+
+    if @book.save
+      redirect_to books_url
+    else
+      render :new
+    end
+  end
+
+  def show
+    find_book
+  end
+
+  def edit
+    find_book
+  end
+
+  def update
+    find_book
+
+    if @book.update(book_params)
+      redirect_to book_url(@book)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    find_book
+
+    if @book.destroy
+      flash[:notice] = "Book deleted"
+      redirect_to books_url
+    else
+      flash[:notice] = "Book could not be deleted"
+      redirect_to books_url
+    end
+  end
+
+  private
+    def find_book
+      @book = Book.find(params[:id])
+    end
+
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root 'books#index'
+
+  get 'books/new', to: 'books#new'
+  post 'books', to: 'books#create'
+  get 'books/:id', to: 'books#show', as: :book
+  get 'books/:id/edit', to: 'books#edit', as: :edit_book
+  put 'books/:id', to: 'books#update'
+  patch 'books/:id', to: 'books#update'
+  delete 'books/:id', to: 'books#destroy'
+
   resources :books, only: [:index]
 end
